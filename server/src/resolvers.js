28a06d8f217a29084c85a5733036b5d8e4a95f92
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2023-06-01 15:56:43 +0100

    feat: add empty resolver for tracks fetching

---
+++ server/src/resolvers.js
@@ -0,0 +1,20 @@
+const resolvers = {
+
+    Query: {
+        /**
+         *  Returns an array of Tracks that will be used to populate
+         *  the homepage grid of our web client
+         * @param parent is the returned value of the resolver for this field's parent.
+         * This will be useful when dealing with resolver chains.
+         * @param args  is an object that contains all GraphQL arguments that were provided for the field by the GraphQL operation,
+         * e.g. the id will be accessible here
+         * @param contextValue is an object shared across all resolvers that are executing for a particular operation.
+         * The resolver needs this argument to share state, like authentication information, a database connection, or in our case the RESTDataSource.
+         * @param info contains information about the operation's execution state, including the field name, the path to the field from the root, and more
+         */
+        tracksForHome: (parent, args, contextValue, info) => {}
+    }
+
+}
+
+module.exports = resolvers
