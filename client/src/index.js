af516e0eb3d38844dd82ffbff6df32e2b56995f6
Matteo Cavalluzzo <matteocavalluzzo@Matteos-MacBook-Air.local>
2023-06-01 13:44:07 +0100

    feat: add client graphql dependency and set ApolloClient

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000", // location of the server
+    cache: new InMemoryCache(), // used to store and reuse query results
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+    //The ApolloProvider component uses React's Context API to make a configured
+    // Apollo Client instance available throughout a React component tree
+    <ApolloProvider client={client}>
+        <GlobalStyles/>
+        <Pages/>
+    </ApolloProvider>,
+    document.getElementById('root')
 );
